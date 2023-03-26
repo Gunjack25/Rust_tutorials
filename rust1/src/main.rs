f691c37d671de1a25d3896cd8930ff5df1bbbd4e
TusharKhajuria52 <thesharkwork@gmail.com>
2023-03-27 02:25:22 +0530

    first change

--- rust1/src/main.rs
+++ rust1/src/main.rs
@@ -1,3 +1,21 @@
+#![allow(unused)]
+
+use std::io; //Adding package input output 
+use std::io::{Write, BufReader, BufRead, ErrorKind};
+use std::fs::File;
+use std::cmp::Ordering;
+
+//Same as C++
 fn main() {
-    println!("Hello, world!");
+    //! stands for macro
+    println!("What is your name?");
+    //mut values can be changed 
+    let mut name: String = String::new();
+    io::stdin().read_line(&mut name) 
+    .expect("Didn't receive Input");
+    //If error then the message is displayed Ok, Error
+    //read_line returns Result which has enum value Ok, Error
+    println!("Hello, {} ", name.trim_end());
+    const PI : f32 = 3.14;
+    let age: &str = "25";
 }
